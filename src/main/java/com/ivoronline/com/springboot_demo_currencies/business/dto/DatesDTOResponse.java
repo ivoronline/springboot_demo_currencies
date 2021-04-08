018c07708b7f5cb866dc6ea7207625e335508e20
igoreta <ivoronline@gmail.com>
2021-04-08 23:08:36 +0200

    Commit

---
+++ src/main/java/com/ivoronline/com/springboot_demo_currencies/business/dto/DatesDTOResponse.java
@@ -0,0 +1,8 @@
+package com.ivoronline.com.springboot_demo_currencies.business.dto;
+
+import java.time.LocalDate;
+
+public class DatesDTOResponse {
+  public LocalDate firstDate;
+  public LocalDate lastDate;
+}
