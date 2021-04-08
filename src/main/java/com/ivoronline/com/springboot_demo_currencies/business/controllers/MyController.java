11f2eccd22560379d290d12a24fa62d0a10346dc
igoreta <ivoronline@gmail.com>
2021-04-08 23:32:02 +0200

    Commit

--- src/main/java/com/ivoronline/com/springboot_demo_currencies/business/controllers/MyController.java
+++ src/main/java/com/ivoronline/com/springboot_demo_currencies/business/controllers/MyController.java
@@ -50,22 +50,4 @@ public class MyController {
     return myService.getAverageValue(currencyName, startDate, endDate);
   }
 
-  //==================================================================
-  // HANDLE EXCEPTIONS (it only catches first exception)
-  //==================================================================
-  @ResponseBody
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  @ExceptionHandler(MissingServletRequestParameterException.class)
-  public String handleExceptions(MissingServletRequestParameterException exception) {
-
-    //GET EXCEPTION DETAILS
-    String parameterType = exception.getParameterType(); //String
-    String parameterName = exception.getParameterName(); //name
-    String message       = exception.getMessage();       //Required String parameter 'name' is not present
-
-    //RETURN MESSAGE
-    return message;
-
-  }
-
 }
