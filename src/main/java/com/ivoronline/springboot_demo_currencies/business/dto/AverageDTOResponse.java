bfd4b9f60ea2d3521b307d6772671778a9a9d2c6
igoreta <ivoronline@gmail.com>
2021-04-09 13:04:50 +0200

    Commit

---
+++ src/main/java/com/ivoronline/springboot_demo_currencies/business/dto/AverageDTOResponse.java
@@ -0,0 +1,5 @@
+package com.ivoronline.springboot_demo_currencies.business.dto;
+
+public class AverageDTOResponse {
+  public Float average;
+}
---
+++ src/test/java/com/ivoronline/springboot_demo_currencies/SpringbootDemoCurrenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_demo_currencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoCurrenciesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
