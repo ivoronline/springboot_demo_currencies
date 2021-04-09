0b13fcc4858130a8845510e51974c1b86d7e8e50
igoreta <ivoronline@gmail.com>
2021-04-09 13:40:40 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_demo_currencies/loggerDB/repositories/LogRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ivoronline.springboot_demo_currencies.loggerDB.repositories;
-
-import com.ivoronline.springboot_demo_currencies.loggerDB.entities.Log;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LogRepository extends CrudRepository<Log, Integer> {  }
