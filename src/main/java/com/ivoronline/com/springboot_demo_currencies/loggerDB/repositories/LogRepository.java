f62bbfb311d375703588d18e65059e0af9308144
igoreta <ivoronline@gmail.com>
2021-04-09 12:07:14 +0200

    Commit

--- src/main/java/com/ivoronline/com/springboot_demo_currencies/loggerDB/repositories/LogRepository.java
+++ src/main/java/com/ivoronline/com/springboot_demo_currencies/loggerDB/repositories/LogRepository.java
@@ -1,6 +1,16 @@
 package com.ivoronline.com.springboot_demo_currencies.loggerDB.repositories;
 
 import com.ivoronline.com.springboot_demo_currencies.loggerDB.entities.Log;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface LogRepository extends CrudRepository<Log, Integer> { }
+import java.time.LocalDate;
+import java.util.List;
+
+public interface LogRepository extends CrudRepository<Log, Integer> {
+
+  //GET LOG
+  @Query(value = "SELECT log FROM Log log WHERE log.username = :username AND log.date >= :startDate AND log.date <= :endDate")
+  List<Log> getLog(String username, LocalDate startDate, LocalDate endDate);
+
+}
